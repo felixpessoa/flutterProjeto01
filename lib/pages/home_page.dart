5540a22d70975d029d1c3099740fada3e77afaa4
felixpessoa <felixpessoa90@gmail.com>
2022-08-02 08:20:23 -0300

    consumindo api metodo get

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,12 +1,51 @@
-
-
 import 'package:flutter/material.dart';
+import 'package:projeto01/models/post_model.dart';
+import 'package:projeto01/repositories/home_repository_imp.dart';
 
-class HomePage extends StatelessWidget {
+import '../controllers/home_controller.dart';
+
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final HomeController _controller = HomeController(HomeRepositoryImp());
+
+  @override
+  void initState() {
+    super.initState();
+    _controller.fetch();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            ValueListenableBuilder<List<PostModel>>(
+              valueListenable: _controller.posts,
+              builder: (_, list, __) {
+                return ListView.builder(
+                  // physics: NeverScrollableScrollPhysics(),
+                  itemCount: list.length,
+                  itemBuilder: (_, idx) => ListTile(
+                    title: Text(list[idx].title),
+                  ),
+                );
+              },
+            ),
+            ElevatedButton(
+              onPressed: () => _controller.fetch(),
+              child: Text('Reload'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
